b7a7d7103de4ae1f1eec46afd24ea059b8db63de
brian lee <cn_brian@163.com>
2023-04-21 00:35:58 +0800

    修改结构

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.knife4jSetup = void 0;
 const express = require("express");
 const path_1 = require("path");
 function knife4jSetup(app, services) {
-    app.use('/', express.static((0, path_1.resolve)(__dirname, '../public/')));
+    app.use('/', express.static(path_1.resolve(__dirname, '../public/')));
     app.use('/swagger-resources', function (req, res) {
         res.send(services);
     });
