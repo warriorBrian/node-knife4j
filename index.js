e60349ff7e18d1e70ce800b96289a40fe7630192
brian lee <cn_brian@163.com>
2023-04-21 01:12:43 +0800

    更新路径

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const path_1 = require("path");
 function knife4jSetup(app, services) {
-    app.use('/', express.static(path_1.resolve(__dirname, '../public/')));
+    app.use('/', express.static(path_1.resolve(__dirname, './public/')));
     app.use('/swagger-resources', function (req, res) {
         res.send(services);
     });
